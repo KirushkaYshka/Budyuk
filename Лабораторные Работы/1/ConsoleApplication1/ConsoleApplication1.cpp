e87ae24382552eaea3f821ced874a2b7eb5a101e
KirushkaYshka <129581673+KirushkaYshka@users.noreply.github.com>
2023-12-07 11:21:51 +0400

    Add files via upload

---
+++ Лабораторные Работы/1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the number of characters in the vector: ";
+    cin >> n;
+
+    vector<char> vec(n);
+    cout << "Enter " << n << " characters for the vector:" << endl;
+    for (int i = 0; i < n; i++) {
+        cin >> vec[i];
+    }
+
+    sort(vec.begin(), vec.end());
+    vector<char>::iterator last = unique(vec.begin(), vec.end());
+    vec.erase(last, vec.end());
+
+    int k;
+    cout << "Enter the index of the element to be removed: ";
+    cin >> k;
+    vec.erase(vec.begin() + k);
+
+    vector<char> vowels = { 'a', 'e', 'i', 'o', 'u' };
+    for (int i = 0; i < vec.size(); i++) {
+        vector<char>::iterator it = find(vowels.begin(), vowels.end(), vec[i]);
+        if (it != vowels.end()) {
+            vec.insert(vec.begin() + i + 1, 'A');
+            i++;
+        }
+    }
+
+    vector<char>::iterator max_it = max_element(vec.begin(), vec.end());
+    iter_swap(vec.begin(), max_it);
+    iter_swap(vec.begin(), vec.end() - 1);
+
+    for (vector<char>::iterator it = vec.begin(); it != vec.end(); it++) {
+        cout << *it << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
---
+++ Лабораторные Работы/2/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <string>
+
+std::string HtmlEncode(std::string const& text) {
+    std::string result;
+    for (char c : text) {
+        switch (c) {
+        case '"':
+            result += "&quot;";
+            break;
+        case '\'':
+            result += "&apos;";
+            break;
+        case '<':
+            result += "&lt;";
+            break;
+        case '>':
+            result += "&gt;";
+            break;
+        case '&':
+            result += "&amp;";
+            break;
+        default:
+            result += c;
+            break;
+        }
+    }
+    return result;
+}
+
+int main() {
+    setlocale(LC_ALL, "RUS");
+    std::string input;
+    std::cout << "Введите текст для функции HTML encode: ";
+    std::getline(std::cin, input);
+    std::string encoded = HtmlEncode(input);
+    std::cout << "Зашифрованый текст: " << encoded << std::endl;
+    return 0;
+}
